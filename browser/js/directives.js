7bb9d0f3e40397fbcf8900f479b95387a662abfd
Samuel Ballan <sgb4622@gmail.com>
2015-10-20 10:17:54 -0400

    finished porting player

--- browser/js/directives.js
+++ browser/js/directives.js
@@ -1,6 +1,29 @@
-app.directive('sidebar2', function() {
+app.directive('sidebar', function() {
   return {
     restrict: 'E',
     templateUrl: '/templates/sidebarTemplate.html'
+  }
+})
+
+
+app.directive('player', function(PlayerFactory) {
+  return {
+    restrict: 'E',
+    templateUrl: '/templates/playerTemplate.html',
+    link: function(scope) {
+      scope.getCurrentSong = PlayerFactory.getCurrentSong;
+    	scope.isPlaying = PlayerFactory.isPlaying;
+    	scope.forward = PlayerFactory.next;
+    	scope.back = PlayerFactory.previous;
+
+    	scope.getPercent = function () {
+    		return (100 * PlayerFactory.getProgress()) + '%';
+    	};
+
+    	scope.toggle = function () {
+    		if (PlayerFactory.isPlaying()) PlayerFactory.pause();
+    		else PlayerFactory.resume();
+    	};
+    }
   }
 })
